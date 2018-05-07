1bf34ef3124483f5fb7e83c887070d9e056c1075
ting wang <ting@guiker.com>
2018-05-06 20:00:09 -0400

    add json api exception

--- src/main/java/org/ting/jsonapi/exception/JsonApiException.java
+++ src/main/java/org/ting/jsonapi/exception/JsonApiException.java
@@ -2,7 +2,7 @@ package org.ting.jsonapi.exception;
 
 import org.ting.jsonapi.ExceptionResponse;
 
-public abstract class JsonApiException extends Exception  implements ExceptionResponse{
+public abstract class JsonApiException extends RuntimeException  implements ExceptionResponse{
 
 	private static final long serialVersionUID = -3756072345831647615L;
 	
